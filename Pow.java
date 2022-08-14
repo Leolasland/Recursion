90ca824bea032bb03327375f7a52e10e935949d1
Svetlana Davletgareeva <hdanyel@student.21-school.ru>
2022-08-14 10:49:32 +0300

    Create Pow.java

---
+++ Pow.java
@@ -0,0 +1,14 @@
+public class Pow {
+
+  public static int pow(int n, int m) {
+    if (m == 1) {
+      return n;
+    } else {
+      return n * pow(n, m - 1);
+    }
+  }
+
+  public static void main(String[] args) {
+    System.out.println(pow(2, 6));
+  }
+}
