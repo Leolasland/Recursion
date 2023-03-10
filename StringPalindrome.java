54c7b3fbe43e9781fd35c1fed96dba69998430e5
Svetlana Davletgareeva <hdanyel@student.21-school.ru>
2023-03-10 10:05:47 +0300

    Update StringPalindrome

--- StringPalindrome.java
+++ StringPalindrome.java
@@ -3,15 +3,18 @@ import java.util.Locale;
 public class StringPalindrome {
 
   public static boolean isPalindrome(String s) {
-    String tmp = s.replaceAll("\\W", "").toLowerCase(Locale.ROOT);
+    String tmp = s.replaceAll("\\W", "");
     int size = tmp.length();
     if (size <= 1) {
       return true;
     }
     char firstChar = tmp.charAt(0);
     char lastChar = tmp.charAt(size - 1);
+    if (firstChar != lastChar) {
+        return false;
+    }
     String substring = tmp.substring(1, size - 1);
-    return firstChar == lastChar && isPalindrome(substring);
+    return isPalindrome(substring);
   }
 
   public static void main(String[] args) {
