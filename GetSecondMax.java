2379b2b24392f17abb4c8f7a742e815f2948df39
Leolasland <hdanyel@student.21-school.ru>
2023-03-12 21:30:49 +0300

    Fixed "get second max" solution.

--- GetSecondMax.java
+++ GetSecondMax.java
@@ -1,19 +1,27 @@
 public class GetSecondMax {
 
-  public static int getSecondMax(int[] array, int max, int max2) {
-    int next = array[0];
-    if (max < next) {
-      getSecondMax(array, next, max);
-    } else if (max2 < next) {
-      getSecondMax(array, max, next);
-    } else {
-      getSecondMax(array, max, max2);
+  public static int getSecondMax(int[] array) {
+    if (array.length <= 0) {
+      return 0;
     }
-    return 0;
+    return secondMax(array, 0, 0, 0);
+  }
+
+  public static int secondMax(int[] array, int max1, int max2, int i) {
+    if (i < array.length) {
+      if (max1 < array[i]) {
+        max2 = secondMax(array, array[i], max2, ++i);
+      } else if (max2 < array[i]) {
+        max2 = secondMax(array, max1, array[i], ++i);
+      } else {
+        max2 = secondMax(array, max1, max2, ++i);
+      }
+    }
+    return max2;
   }
 
   public static void main(String[] args) {
-    int[] array = {5, 4, 3, 2, 1};
-    System.out.println(getSecondMax(array, 0, 0));
+    int[] array = {5, 4, 3, 2, 5};
+    System.out.println(getSecondMax(array));
   }
 }
